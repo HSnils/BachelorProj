cb84915b67534d5769362f91d9c95a765ac919ef
HSnils <hrsnils@gmail.com>
2018-03-05 11:26:08 +0100

    namefix

--- database/migrations/2018_03_02_113527_create_categories_table.php
+++ database/migrations/2018_03_02_113527_create_categories_table.php
@@ -15,8 +15,8 @@ class CreateCategoriesTable extends Migration
     {
         Schema::create('categories', function (Blueprint $table) {
 			  
-            $table->string('name', 30)->primary();
-			  	$table->string('type', 30);
+            $table->string('category', 30)->primary();
+			$table->string('type', 30);
 			  
             $table->timestamps();
 			  
