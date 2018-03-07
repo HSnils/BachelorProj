c89ddb86da19aa837da5fcfaf9a4968a3b913777
HSnils <hrsnils@gmail.com>
2018-03-07 14:49:52 +0100

    tooltip select role

--- resources/views/admin/newBookingsTable.blade.php
+++ resources/views/admin/newBookingsTable.blade.php
@@ -1,8 +1,11 @@
 <table class="mdl-data-table mdl-js-data-table mdl-shadow--2dp ">
 			<thead>
 				<tr>
-					<th class="mdl-data-table__cell--non-numeric">Room Number</th>
+					<th class="mdl-data-table__cell--non-numeric">Type</th>
+					<th class="mdl-data-table__cell--non-numeric">Name</th>
 					<th class="mdl-data-table__cell--non-numeric">User</th>
+					<th>From</th>
+					<th>To</th>
 					<th class="mdl-data-table__cell--non-numeric">Accept</th>
 					<th class="mdl-data-table__cell--non-numeric">Reject</th>
 				</tr>
@@ -10,11 +13,20 @@
 			<tbody>
 				@foreach ($newBookings as $booking)
 					<tr>
+						<td>
+							{{$booking->type}}
+						</td>
 						<td class="mdl-data-table__cell--non-numeric">
 							{{$booking->room_number}}
-						</td class="mdl-data-table__cell--non-numeric">
-						<td>
+						</td >
+						<td class="mdl-data-table__cell--non-numeric">
 							{{$booking->name}}
+						</td>
+						<td>
+							{{$booking->from_date}}
+						</td>
+						<td>
+							{{$booking->to_date}}
 						</td>
 						<td class="mdl-data-table__cell--non-numeric">
 							<a href="#"><i class="material-icons">done</i></a>
