2636034726f574557e6ca2a122d111458441664b
HSnils <hrsnils@gmail.com>
2018-03-12 14:34:58 +0100

    fixed bug with navigation on smaller screens

--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -1,59 +1,60 @@
-        <!-- Always shows a header, even in smaller screens. -->
+		<!-- Always shows a header, even in smaller screens. -->
 <div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
-  <header class="mdl-layout__header">
-    <div class="mdl-layout__header-row">
-      <!-- Title -->
-      <span class="mdl-layout-title">Cololab NTNU</span>
-      <!-- Add spacer, to align navigation to the right -->
-      <div class="mdl-layout-spacer"></div>
-      <!-- Navigation. We hide it in small screens. -->
-      
-       <!-- Checks if the user hasnt logged in-->
-        @if (Auth::guest())
-          	<hr>
-  			<a class="mdl-navigation__link" href="{{ route('login') }}">Sign in</a>
-        @else
-        	<!-- Show this if user is logged in -->
-	        <a class="mdl-navigation__link" href="{{ route('home') }}">Home</a>
-	        <a class="mdl-navigation__link" href="{{ route('rooms') }}">Rooms</a>
-	        <a class="mdl-navigation__link" href="{{ route('equipments') }}">Equipments</a>
-			<a class="mdl-navigation__link" href="{{ route('profile') }}">Profile</a>
-			
-			<!-- Show this if the user is admin -->
-			@if (Auth::user()->role == 'Admin')
-			<a href="{{ route('admin') }}" class="mdl-navigation__link">
-				Admin
-			</a>
-			@endif
-			
-			<!--SIGN OUT -->
-			<hr>
-			<a class="mdl-navigation__link" href="{{ route('logout') }}" onclick="
-				event.preventDefault();
-				document.getElementById('logout-form').submit();">
-			Sign out
-        	</a>
-			<form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-			    {{ csrf_field() }}
-			</form>
-        @endif
+	<header class="mdl-layout__header">
+	<div class="mdl-layout__header-row">
+		<!-- Title -->
+		<span class="mdl-layout-title">Cololab NTNU</span>
+		<!-- Add spacer, to align navigation to the right -->
+		<div class="mdl-layout-spacer"></div>
 
-      </nav>
-    </div>
-  </header>
-  <div class="mdl-layout__drawer">
-    <span class="mdl-layout-title">Colorlab</span>
-    <nav class="mdl-navigation">
-        
-        <!-- Checks if the user hasnt logged in-->
-        @if (Auth::guest())
-          	<hr>
-  			<a class="mdl-navigation__link" href="{{ route('login') }}">Sign in</a>
-        @else
-        	<!-- Show this if user is logged in -->
-	        <a class="mdl-navigation__link" href="{{ route('home') }}">Home</a>
-	        <a class="mdl-navigation__link" href="{{ route('rooms') }}">Rooms</a>
-	        <a class="mdl-navigation__link" href="{{ route('equipments') }}">Equipments</a>
+		<!-- Navigation. We hide it in small screens. -->
+		<nav class="mdl-navigation mdl-layout--large-screen-only">
+			<!-- Checks if the user hasnt logged in-->
+			@if (Auth::guest())
+				<hr>
+				<a class="mdl-navigation__link" href="{{ route('login') }}">Sign in</a>
+			@else
+				<!-- Show this if user is logged in -->
+				<a class="mdl-navigation__link" href="{{ route('home') }}">Home</a>
+				<a class="mdl-navigation__link" href="{{ route('rooms') }}">Rooms</a>
+				<a class="mdl-navigation__link" href="{{ route('equipments') }}">Equipments</a>
+				<a class="mdl-navigation__link" href="{{ route('profile') }}">Profile</a>
+				
+				<!-- Show this if the user is admin -->
+				@if (Auth::user()->role == 'Admin')
+				<a href="{{ route('admin') }}" class="mdl-navigation__link">
+					Admin
+				</a>
+				@endif
+				
+				<!--SIGN OUT -->
+				<hr>
+				<a class="mdl-navigation__link" href="{{ route('logout') }}" onclick="
+					event.preventDefault();
+					document.getElementById('logout-form').submit();">
+				Sign out
+				</a>
+				<form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+					{{ csrf_field() }}
+				</form>
+			@endif
+
+		</nav>
+	</div>
+	</header>
+	<div class="mdl-layout__drawer">
+	<span class="mdl-layout-title">Colorlab</span>
+	<nav class="mdl-navigation">
+		
+		<!-- Checks if the user hasnt logged in-->
+		@if (Auth::guest())
+			<hr>
+			<a class="mdl-navigation__link" href="{{ route('login') }}">Sign in</a>
+		@else
+			<!-- Show this if user is logged in -->
+			<a class="mdl-navigation__link" href="{{ route('home') }}">Home</a>
+			<a class="mdl-navigation__link" href="{{ route('rooms') }}">Rooms</a>
+			<a class="mdl-navigation__link" href="{{ route('equipments') }}">Equipments</a>
 			<a class="mdl-navigation__link" href="{{ route('profile') }}">Profile</a>
 			
 			<!-- Show this if the user is admin -->
@@ -69,11 +70,11 @@
 				event.preventDefault();
 				document.getElementById('logout-form-sidebar').submit();">
 			Sign out
-        	</a>
+			</a>
 			<form id="logout-form-sidebar" action="{{ route('logout') }}" method="POST" style="display: none;">
-			    {{ csrf_field() }}
+				{{ csrf_field() }}
 			</form>
-        @endif
-      </nav>
+		@endif
+	</nav>
   </div>
   <main class="mdl-layout__content">
--- resources/views/partials/navAdmin.blade.php
+++ resources/views/partials/navAdmin.blade.php
@@ -7,29 +7,31 @@
 		<!-- Add spacer, to align navigation to the right -->
 		<div class="mdl-layout-spacer"></div>
 		<!-- Navigation. We hide it in small screens. -->
+		<nav class="mdl-navigation mdl-layout--large-screen-only">
+			
+			<!-- Show this if user is logged in -->
+			<a class="mdl-navigation__link" href="{{ route('admin') }}">Dashboard</a>
+			<a class="mdl-navigation__link" href="{{ route('roomsAdmin') }}">Rooms</a>
+			<a class="mdl-navigation__link" href="{{ route('equipmentsAdmin') }}">Equipments</a>
+			<a class="mdl-navigation__link" href="{{ route('users') }}">Users</a>
+			<a class="mdl-navigation__link" href="{{ route('logg') }}">Logg</a>
 
-		<!-- Show this if user is logged in -->
-		<a class="mdl-navigation__link" href="{{ route('roomsAdmin') }}">Rooms</a>
-		<a class="mdl-navigation__link" href="{{ route('equipmentsAdmin') }}">Equipments</a>
-		<a class="mdl-navigation__link" href="{{ route('users') }}">Users</a>
-		<a class="mdl-navigation__link" href="{{ route('logg') }}">Logg</a>
 
+			<a href="{{ route('home') }}" class="mdl-navigation__link">
+			Back to normal page
+			</a>
+			
 
-		<a href="{{ route('home') }}" class="mdl-navigation__link">
-		Back to normal page
-		</a>
-		
-
-		<!--SIGN OUT -->
-		<hr>
-		<a class="mdl-navigation__link" href="{{ route('logout') }}" onclick="
-		event.preventDefault();
-		document.getElementById('logout-form').submit();">
-			Sign out
-		</a>
-		<form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-			{{ csrf_field() }}
-		</form>
+			<!--SIGN OUT -->
+			<hr>
+			<a class="mdl-navigation__link" href="{{ route('logout') }}" onclick="
+			event.preventDefault();
+			document.getElementById('logout-form').submit();">
+				Sign out
+			</a>
+			<form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+				{{ csrf_field() }}
+			</form>
 
 		</nav>
     </div>
@@ -38,35 +40,29 @@
     <span class="mdl-layout-title">Colorlab</span>
     <nav class="mdl-navigation">
         
-        <!-- Checks if the user hasnt logged in-->
-        @if (Auth::guest())
-          	<hr>
-  			<a class="mdl-navigation__link" href="{{ route('login') }}">Sign in</a>
-        @else
-        	<!-- Show this if user is logged in -->
-	        <a class="mdl-navigation__link" href="{{ route('home') }}">Home</a>
-	        <a class="mdl-navigation__link" href="{{ route('rooms') }}">Rooms</a>
-	        <a class="mdl-navigation__link" href="{{ route('equipments') }}">Equipments</a>
-			<a class="mdl-navigation__link" href="{{ route('profile') }}">Profile</a>
-			
-			<!-- Show this if the user is admin -->
-			@if (Auth::user()->role == 'Admin')
-			<a href="{{ route('admin') }}" class="mdl-navigation__link">
-				Admin
+        <!-- Show this if user is logged in -->
+        	<a class="mdl-navigation__link" href="{{ route('admin') }}">Dashboard</a>
+			<a class="mdl-navigation__link" href="{{ route('roomsAdmin') }}">Rooms</a>
+			<a class="mdl-navigation__link" href="{{ route('equipmentsAdmin') }}">Equipments</a>
+			<a class="mdl-navigation__link" href="{{ route('users') }}">Users</a>
+			<a class="mdl-navigation__link" href="{{ route('logg') }}">Logg</a>
+
+
+			<a href="{{ route('home') }}" class="mdl-navigation__link">
+			Back to normal page
 			</a>
-			@endif
 			
+
 			<!--SIGN OUT -->
 			<hr>
 			<a class="mdl-navigation__link" href="{{ route('logout') }}" onclick="
-				event.preventDefault();
-				document.getElementById('logout-form-sidebar').submit();">
-			Sign out
-        	</a>
-			<form id="logout-form-sidebar" action="{{ route('logout') }}" method="POST" style="display: none;">
-			    {{ csrf_field() }}
+			event.preventDefault();
+			document.getElementById('logout-form').submit();">
+				Sign out
+			</a>
+			<form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+				{{ csrf_field() }}
 			</form>
-        @endif
       </nav>
   </div>
   <main class="mdl-layout__content">
