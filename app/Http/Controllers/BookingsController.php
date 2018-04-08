e31a8f285b26e210ebb4f7ea1a041077129bc882
HSnils <hrsnils@gmail.com>
2018-04-09 01:03:31 +0200

    fixed check to find if booking is avalible
    using whereBetween instead

--- app/Http/Controllers/BookingsController.php
+++ app/Http/Controllers/BookingsController.php
@@ -13,6 +13,11 @@ use Auth;
 
 class BookingsController extends Controller
 {
+	/**
+	 * [create description]
+	 * @param  Request $request [all inputs from the form]
+	 * @return [Response]           [return either successfull or declined booking]
+	 */
 	public function create(Request $request){
 		//validates input (checks if they are filled in)
 		$this->validate(request(), [
@@ -51,9 +56,16 @@ class BookingsController extends Controller
 		}
 
 		//need to fix query add between stuff - checks if room is avalible
-		$checkRoomAvalibility = Bookings::join('bookings_rooms', 'bookings.id', '=', 'bookings_rooms.bookings_id')->join('rooms', 'bookings_rooms.room_number', '=', 'rooms.room_number')->where('from_date', '<=', $dateTo)->where('to_date','>=', $dateFrom)->where('rooms.room_number', '=', $roomNumber)->count();
+		/*$checkRoomAvalibility = Bookings::join('bookings_rooms', 'bookings.id', '=', 'bookings_rooms.bookings_id')->join('rooms', 'bookings_rooms.room_number', '=', 'rooms.room_number')->where('from_date', '<=', $dateTo)->where('to_date','>=', $dateFrom)->where('rooms.room_number', '=', $roomNumber)->count();*/
 
-		
+		$checkRoomAvalibility = Bookings::
+			join('bookings_rooms', 'bookings.id', '=', 'bookings_rooms.bookings_id')
+			->join('rooms', 'bookings_rooms.room_number', '=', 'rooms.room_number')
+			->where('rooms.room_number', '=', $roomNumber)
+			->whereBetween('from_date', [$dateFrom, $dateTo])
+			->whereBetween('to_date', [$dateFrom, $dateTo])
+			->count();
+
 		//if checkRoomAvalibility == 0 there was no other bookings and the room is avalible
 		if($checkRoomAvalibility == 0){
 			$roomAvalible = true;
@@ -69,7 +81,13 @@ class BookingsController extends Controller
 			for($i = 0; $i < count($equipmentsArray); $i++){
 				$equipment_id = $equipmentsArray[$i];
 
-				$equipmentCheck = Bookings::join('bookings_equipments', 'bookings.id', '=', 'bookings_equipments.bookings_id')->join('equipments', 'bookings_equipments.equipment_id', '=', 'equipments.id')->where('from_date', '<=', $dateTo)->where('to_date','>=', $dateFrom)->where('equipments.id', $equipment_id)->count();
+				$equipmentCheck = Bookings::
+					join('bookings_equipments', 'bookings.id', '=', 'bookings_equipments.bookings_id')
+					->join('equipments', 'bookings_equipments.equipment_id', '=', 'equipments.id')
+					->where('equipments.id', $equipment_id)
+					->whereNotBetween('from_date', [$dateFrom, $dateTo])
+					->whereNotBetween('to_date', [$dateFrom, $dateTo])
+					->count();
 
 				//if there was another equipment booking at the same time, change equipments avalible to false
 				if($equipmentCheck >= 1){
@@ -81,6 +99,7 @@ class BookingsController extends Controller
 			return true;
 		}
 
+		//runs function to check avalibility of equipments if number of equipments are more than or is equal to 1
 		if($numberOfEquipments >= 1){
 			$equimentsAvalible = checkEquipmentAvalibility($equipmentsArray, $dateTo, $dateFrom);
 		}
@@ -113,7 +132,7 @@ class BookingsController extends Controller
 				session()->flash('notifyUser', 'Room booked!');
 
 			} else {
-				//error
+				session()->flash('notifyUser', 'Room is not avalible!');
 			}
 		} else if($roomAvalible == true &&  $equimentsAvalible == true){
 			//if room is avalible and there is an equipment selected
@@ -171,12 +190,18 @@ class BookingsController extends Controller
 
 	}
 
+	//when room is selected in the dropdown sends a ajax request with the room to get the list of equipments 
 	public function roomSelected($room){
 		$getEquipmentsInRoom = Equipments::where('location', $room)->get();
 
 		return json_encode($getEquipmentsInRoom);
 	}
 
+	/**
+	 * [delete description]
+	 * @param  [type] $booking [filled with booking id variable]
+	 * @return [type]          [deletes booking if user is admin]
+	 */
 	public function delete( $booking){
 		$isAdmin = auth()->user()->role == 'Admin';
 		//if statement to check if $isAdmin is true
@@ -195,6 +220,11 @@ class BookingsController extends Controller
 		
 	}
 
+	/**
+	 * [approve description]
+	 * @param  [type] $booking [filled with booking id ]
+	 * @return [type]          [approves new booking]
+	 */
 	public function approve( $booking){
 		$isAdmin = auth()->user()->role == 'Admin';
 		//if statement to check if $isAdmin is true
