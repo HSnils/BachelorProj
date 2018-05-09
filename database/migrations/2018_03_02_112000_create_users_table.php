1a66dedc8231c7097eb7c2f8c25558729916488c
Henrik Reff Snilsberg <hrsnils@gmail.com>
2018-05-09 14:31:17 +0200

    login email fix
    forgot to fix login just fixed registration

--- database/migrations/2018_03_02_112000_create_users_table.php
+++ database/migrations/2018_03_02_112000_create_users_table.php
@@ -15,25 +15,25 @@ class CreateUsersTable extends Migration
 	{
 		Schema::create('users', function (Blueprint $table) {
 
-		$table->increments('id');
-		$table->string('name', 30);
-		$table->string('email', 100)->unique();
-		$table->string('password', 255);
-		$table->string('role', 30)->default('Guest');
-		$table->string('token', 100)->nullable();
-    	$table->boolean('verified')->default(false);
-		$table->boolean('mailSent')->default(false);
-		$table->string('status', 30)->default('Active');
-		$table->string('card_id', 50)->nullable();
-		$table->dateTime('last_login');
-		$table->timestamps();
-		$table->string('updated_by', 30)->nullable();
-		$table->dateTime('deleted_at')->nullable();
-		$table->string('deleted_by', 30)->nullable();
-		$table->rememberToken();
+			$table->increments('id');
+			$table->string('name', 30);
+			$table->string('email', 100)->unique();
+			$table->string('password', 255);
+			$table->string('role', 30)->default('Guest');
+			$table->string('token', 100)->nullable();
+	    	$table->boolean('verified')->default(false);
+			$table->boolean('mailSent')->default(false);
+			$table->string('status', 30)->default('Active');
+			$table->string('card_id', 50)->nullable();
+			$table->dateTime('last_login');
+			$table->timestamps();
+			$table->string('updated_by', 30)->nullable();
+			$table->dateTime('deleted_at')->nullable();
+			$table->string('deleted_by', 30)->nullable();
+			$table->rememberToken();
 
 
-		$table->foreign('role')->references('role')->on('roles')->onUpdate('cascade')->onDelete('cascade');
+			$table->foreign('role')->references('role')->on('roles')->onUpdate('cascade')->onDelete('cascade');
 		});
 	}
 
