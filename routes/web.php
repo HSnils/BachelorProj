2af5e43e69e37202eafecb36ff70b40f12e6546d
Henrik Reff Snilsberg <hrsnils@gmail.com>
2018-05-15 13:32:15 +0200

    delete room remov

--- routes/web.php
+++ routes/web.php
@@ -43,7 +43,6 @@ Route::delete('bookings/delete/{booking}', 'BookingsController@userDelete')->mid
 //users report damage to equipement
 Route::post('/equipments/{id}', 'EquipmentsController@reportDamage')->middleware('auth');
 
-
 //---- ADMIN STUFF ----//
 // Routes to Admin views
 Route::get('/admin', 'AdminController@index')->name('admin')->middleware('auth');
@@ -71,7 +70,7 @@ Route::get('admin/rooms/newRoom', 'RoomsController@newRoom')->name('newRoom')->m
 Route::post('admin/rooms/create', 'RoomsController@createRoom')->middleware('auth');
 Route::get('admin/rooms/edit/{room_number}', 'RoomsController@showEdit')->middleware('auth');
 Route::post('admin/rooms/edit', 'RoomsController@editRoom')->middleware('auth');
-Route::delete('admin/rooms/delete/{room_number}', 'RoomsController@delete')->middleware('auth');
+//Route::delete('admin/rooms/delete/{room_number}', 'RoomsController@delete')->middleware('auth');
 
 //Admin - Create/edit equipments
 Route::get('admin/equipments/newEquipment', 'EquipmentsController@newEquipment')->name('newEquipment')->middleware('auth');
